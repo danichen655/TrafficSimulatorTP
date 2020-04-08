d7dfd84e5f67e406854a600e82bd95c1c92d17d6
Adrián Turiel Charro <60858106+atuch17@users.noreply.github.com>
2020-04-08 19:42:21 +0200

    modificacion parte 1 practicamente completa

--- src/simulator/model/TrafficSimObserver.java
+++ src/simulator/model/TrafficSimObserver.java
@@ -2,7 +2,7 @@ package simulator.model;
 
 import java.util.List;
 
-public interface TrafficSimObserver {
+public interface TrafficSimObserver {		//TODO esto se mete dentro de otra clase o aqui esta bien
 	void onAdvanceStart(RoadMap map, List<Event> events, int time);
 
 	void onAdvanceEnd(RoadMap map, List<Event> events, int time);
--- src/simulator/model/TrafficSimulator.java
+++ src/simulator/model/TrafficSimulator.java
@@ -1,5 +1,6 @@
 package simulator.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONObject;
@@ -8,32 +9,40 @@ import exceptions.NegativeException;
 import exceptions.StatusException;
 import simulator.misc.SortedArrayList;
 
-public class TrafficSimulator {
+public class TrafficSimulator implements Observable<TrafficSimObserver>{
 
 	private RoadMap mapaCarreteras; // guarda todos los objetos de la simulacion
 	private List<Event> listaEventos; // usar SortedArrayList
 	private int tiempo;
-
+	//PARAMETROS NUEVOS
+	private List<TrafficSimObserver> observerList;
+	
+	
+	
 	public TrafficSimulator() {
 		super();
 		this.mapaCarreteras = new RoadMap();
 		this.listaEventos = new SortedArrayList<Event>();
 		this.tiempo = 0;
+		this.observerList = new ArrayList<TrafficSimObserver>();
 	}
 
 	public void addEvent(Event e) {
 		listaEventos.add(e);
+		//
+		notifyOnEventAdded(e);
 	}
 
-	public void advance() throws NegativeException, StatusException {
+	public void advance() throws NegativeException, StatusException {		
 		// 1
 		tiempo++;
+		
+		//
+		notifyOnAdvanceStart();
 		// 2
 
 		while (listaEventos.size() > 0 && listaEventos.get(0).getTime() == this.tiempo) {
-
 			listaEventos.remove(0).execute(mapaCarreteras);
-
 		}
 
 		// 3
@@ -43,14 +52,17 @@ public class TrafficSimulator {
 		// 4
 		for (Road carreteras : mapaCarreteras.getRoads()) {
 			carreteras.advance(tiempo);
-		}
-
+		}	
+		
+		//
+		notifyOnAdvanceEnd();
 	}
 
 	public void reset() {
 		tiempo = 0;
 		mapaCarreteras.reset();
 		listaEventos.clear();
+		notifyOnReset();
 	}
 
 	public JSONObject report() {
@@ -59,5 +71,47 @@ public class TrafficSimulator {
 		jo.put("state", mapaCarreteras.report());
 		return jo;
 	}
+	
+	//PARTE NUEVA
+	
+	@Override
+	public void addObserver(TrafficSimObserver o) {
+		this.observerList.add(o);		
+	}
 
+	@Override
+	public void removeObserver(TrafficSimObserver o) {
+		this.observerList.remove(o);		
+	}	
+
+	public void notifyOnAdvanceStart() {
+		for(TrafficSimObserver ob:observerList)
+			ob.onAdvanceStart(mapaCarreteras, listaEventos, tiempo);
+	} 	
+
+	public void notifyOnAdvanceEnd() {
+		for(TrafficSimObserver ob:observerList)
+			ob.onAdvanceEnd(mapaCarreteras, listaEventos, tiempo);
+	}
+
+	public void notifyOnEventAdded(Event e) {
+		for(TrafficSimObserver ob:observerList)
+			ob.onEventAdded(mapaCarreteras, listaEventos, e, tiempo);
+	}	
+
+	public void notifyOnReset() {
+		for(TrafficSimObserver ob:observerList)
+			ob.onReset(mapaCarreteras, listaEventos, tiempo);
+	}
+
+	public void notifyOnError(String err) {	//TODO revisar xq esta mal, ver desde donde llamarla, y hacer el throw
+		try {
+			for(TrafficSimObserver ob:observerList)
+				ob.onError(err);
+		
+		}
+		catch(Exception ex){
+			notifyOnError(ex.getMessage());
+		}
+	}
 }
